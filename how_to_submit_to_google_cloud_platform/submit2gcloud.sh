09d31e95dd7f88aee3d45c6dcbd0a14db2d439fa
Badri Adhikari <badri.com.np@gmail.com>
2019-04-05 19:23:48 -0500

    Delete submit2gcloud.sh

--- how_to_submit_to_google_cloud_platform/submit2gcloud.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATE_WITH_TIME=`date "+%Y_%m_%d_%H_%M_%S"`
-
-./google-cloud-sdk/bin/gcloud ml-engine jobs submit training job_$DATE_WITH_TIME \
-    --module-name=trainer.train --package-path=./trainer --job-dir=gs://dist-predict-test \
-    --region=us-central1 --config=cloudml.yaml
